6985644ecb27eae68ee753f27d701cc98c3a45ed
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-04 11:13:08 +0900

    ヒット数を返すメソッドの追加

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -12,14 +12,13 @@
     Public Function ReturnResultOfHitAndBlowGame(playerAnswer As Integer) As String
         Dim computerNumber As New List(Of Char)(New Char() {"1"c, "2"c, "3"c, "4"c})
         Dim playerNumber As New List(Of Char)(playerAnswer.ToString.ToCharArray)
-        Dim hit As Integer = 0
         Dim blow As Integer = 0
         'hitした数字はblowでは比較しないので削除します。hitの位置を記録します
         Dim hitIndexs As New List(Of Integer)
 
+        Dim hit As Integer = CountNumberOfHit(computerNumber, playerNumber)
         For i As Integer = 0 To computerNumber.Count - 1
             If computerNumber(i) = playerNumber(i) Then
-                hit += 1
                 hitIndexs.Add(i)
             End If
         Next
@@ -47,6 +46,25 @@
 
         Dim result As String = ("Hit:" & hit.ToString & "," & "Blow:" & blow.ToString)
         Return result
+
+    End Function
+
+    ''' <summary>
+    ''' 受け取った2つのリストを比較し、数字が同じであればhitに1を足す
+    ''' </summary>
+    ''' <param name="computerNumber"></param>
+    ''' <param name="playerNumber"></param>
+    ''' <returns></returns>
+    Public Function CountNumberOfHit(computerNumber As List(Of Char), playerNumber As List(Of Char)) As Integer
+        Dim hit As Integer = 0
+
+        For i As Integer = 0 To computerNumber.Count - 1
+            If computerNumber(i) = playerNumber(i) Then
+                hit += 1
+            End If
+        Next
+
+        Return hit
 
     End Function
 
