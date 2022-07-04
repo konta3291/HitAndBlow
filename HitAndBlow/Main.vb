bb6cd5864eea057982a8a5715df46f0973528ff1
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-04 11:00:38 +0900

    ヒット＆ブローの処理追加

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -1,7 +1,53 @@
-﻿Module Main
+﻿Public Module Main
 
     Sub Main()
 
     End Sub
+
+    ''' <summary>
+    ''' 数字を受け取りヒット＆ブローの結果を返す
+    ''' </summary>
+    ''' <param name="playerAnswer"></param>
+    ''' <returns></returns>
+    Public Function ReturnResultOfHitAndBlowGame(playerAnswer As Integer) As String
+        Dim computerNumber As New List(Of Char)(New Char() {"1"c, "2"c, "3"c, "4"c})
+        Dim playerNumber As New List(Of Char)(playerAnswer.ToString.ToCharArray)
+        Dim hit As Integer = 0
+        Dim blow As Integer = 0
+        'hitした数字はblowでは比較しないので削除します。hitの位置を記録します
+        Dim hitIndexs As New List(Of Integer)
+
+        For i As Integer = 0 To computerNumber.Count - 1
+            If computerNumber(i) = playerNumber(i) Then
+                hit += 1
+                hitIndexs.Add(i)
+            End If
+        Next
+
+        If hit < 4 Then
+
+            '反転させ数字の大きい方から削除します
+            hitIndexs.Reverse()
+            For Each hitIndex As Integer In hitIndexs
+                computerNumber.RemoveAt(hitIndex)
+                playerNumber.RemoveAt(hitIndex)
+            Next
+
+            For j As Integer = 0 To computerNumber.Count - 1
+                For k As Integer = 0 To playerNumber.Count - 1
+                    If computerNumber(j) = playerNumber(k) Then
+                        blow += 1
+                        playerNumber.RemoveAt(k)
+                        Exit For
+                    End If
+                Next
+            Next
+
+        End If
+
+        Dim result As String = ("Hit:" & hit.ToString & "," & "Blow:" & blow.ToString)
+        Return result
+
+    End Function
 
 End Module
--- HitAndBlowTest/HitAndBlow.Test.vb
+++ HitAndBlowTest/HitAndBlow.Test.vb
@@ -1,3 +1,33 @@
-﻿Public Class Class1
+﻿Imports HitAndBlow
+Imports NUnit.Framework
+<TestFixture> Public Class HitAndBlowTest
+
+    <Test()> Public Sub ヒット4ブロー0の時()
+
+        Dim result As String = Main.ReturnResultOfHitAndBlowGame(1234)
+        Assert.AreEqual("Hit:4,Blow:0", result)
+
+    End Sub
+
+    <Test()> Public Sub ヒット2ブロー2の時()
+
+        Dim result As String = Main.ReturnResultOfHitAndBlowGame(1324)
+        Assert.AreEqual("Hit:2,Blow:2", result)
+
+    End Sub
+
+    <Test()> Public Sub ヒットもブローも0の時()
+
+        Dim result As String = Main.ReturnResultOfHitAndBlowGame(5678)
+        Assert.AreEqual("Hit:0,Blow:0", result)
+
+    End Sub
+
+    <Test()> Public Sub ヒット0のブロー4()
+
+        Dim result As String = Main.ReturnResultOfHitAndBlowGame(4321)
+        Assert.AreEqual("Hit:0,Blow:4", result)
+
+    End Sub
 
 End Class
