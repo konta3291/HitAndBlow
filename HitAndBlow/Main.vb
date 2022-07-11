7e88a8cf77c05804dd5122920bb51eeff12fcd0d
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-11 13:40:44 +0900

    もう一度遊ぶかの回答をする際、文言はY、N大文字で聞いているのに、小文字のｙ、ｎしか反応しないようになっていたため、Ｙ，Ｎに変更

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -18,11 +18,11 @@ Public Module Main
     Private Function AskPlayerWantToPlayAgain() As Boolean
         Dim playerAnswer As String = ""
 
-        While Not (playerAnswer.Equals("y") OrElse playerAnswer.Equals("n"))
+        While Not (playerAnswer.Equals("Y") OrElse playerAnswer.Equals("N"))
             Console.WriteLine("もう一度プレイしますか？（Y/N）")
             playerAnswer = Console.ReadLine()
         End While
-        Return playerAnswer.Equals("y")
+        Return playerAnswer.Equals("Y")
     End Function
 
     ''' <summary>
