c143e1b9e592a8b9e2eb8380dca0c8a47af5bf63
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-13 13:16:09 +0900

    テスト名を内容の伝わる名前に変更

--- HitAndBlowTest/MainTest.vb
+++ HitAndBlowTest/MainTest.vb
@@ -2,31 +2,31 @@
 Imports NUnit.Framework
 <TestFixture> Public MustInherit Class MainTest
     Public Class IsNumbersAreCorrectForGameTest : Inherits MainTest
-        <Test()> Public Sub 四桁の数字を渡したときのテスト()
+        <Test()> Public Sub 四桁の数字を渡したときはTrueと返すかテスト()
 
             Assert.IsTrue(Main.IsNumbersAreCorrectForGame("0000"))
 
         End Sub
 
-        <Test()> Public Sub 五桁の数字を渡したときのテスト()
+        <Test()> Public Sub 四桁ではない五桁の数字を渡したときはFalseと返すかテスト()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame("00000"))
 
         End Sub
 
-        <Test()> Public Sub 空文字を渡したときのテスト()
+        <Test()> Public Sub 四桁の数字ではない空文字を渡したときはFalseと返すかテスト()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame(""))
 
         End Sub
 
-        <Test()> Public Sub 数字以外の文字を含むときのテスト()
+        <Test()> Public Sub 数字以外の文字を含むときはFalseと返すかテスト()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame("1.56"))
 
         End Sub
 
-        <Test()> Public Sub 数字以外の文字を渡したときのテスト()
+        <Test()> Public Sub 数字以外の文字を渡したときはFalseと返すかテスト()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame("aaaa"))
 
