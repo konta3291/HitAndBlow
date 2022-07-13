fc5e51d33849a36c185a9860a165deee69702d18
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-13 14:35:27 +0900

    テストメソッド名の無駄なところを削除

--- HitAndBlowTest/MainTest.vb
+++ HitAndBlowTest/MainTest.vb
@@ -2,31 +2,31 @@
 Imports NUnit.Framework
 <TestFixture> Public MustInherit Class MainTest
     Public Class IsNumbersAreCorrectForGameTest : Inherits MainTest
-        <Test()> Public Sub 四桁の数字を渡したときはTrueと返すかテスト()
+        <Test()> Public Sub 四桁の数字を渡したときはTrue()
 
             Assert.IsTrue(Main.IsNumbersAreCorrectForGame("0000"))
 
         End Sub
 
-        <Test()> Public Sub 四桁ではない五桁の数字を渡したときはFalseと返すかテスト()
+        <Test()> Public Sub 四桁ではない五桁の数字を渡したときはFalse()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame("00000"))
 
         End Sub
 
-        <Test()> Public Sub 四桁の数字ではない空文字を渡したときはFalseと返すかテスト()
+        <Test()> Public Sub 四桁の数字ではない空文字を渡したときはFalse()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame(""))
 
         End Sub
 
-        <Test()> Public Sub 数字以外の文字を含むときはFalseと返すかテスト()
+        <Test()> Public Sub 数字以外の文字を含むときはFalse()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame("1.56"))
 
         End Sub
 
-        <Test()> Public Sub 数字以外の文字を渡したときはFalseと返すかテスト()
+        <Test()> Public Sub 数字以外の文字を渡したときはFalse()
 
             Assert.IsFalse(Main.IsNumbersAreCorrectForGame("aaaa"))
 
