8509be2aa60d7384030e1de9a445399114097a68
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-14 16:01:08 +0900

    ヒット数を返すメソッドの追加

---
+++ HitAndBlow/HitAndBlowGame.vb
@@ -0,0 +1,22 @@
+﻿Public Class HitAndBlowGame
+
+    ''' <summary>
+    ''' 受け取った2つのリストを比較し、位置と数字が同じものがいくつあったかカウントしその値を返す
+    ''' </summary>
+    ''' <param name="computerNumber"></param>
+    ''' <param name="playerNumber"></param>
+    ''' <returns></returns>
+    Public Function CountNumberOfHit(computerNumber As List(Of Char), playerNumber As List(Of Char)) As Integer
+        Dim hit As Integer = 0
+
+        For i As Integer = 0 To computerNumber.Count - 1
+            If computerNumber(i) = playerNumber(i) Then
+                hit += 1
+            End If
+        Next
+
+        Return hit
+
+    End Function
+
+End Class
---
+++ HitAndBlowTest/HitAndBlowGameTest.vb
@@ -0,0 +1,21 @@
+﻿Imports HitAndBlow
+Imports NUnit.Framework
+<TestFixture> Public MustInherit Class HitAndBlowGameTest
+    Private sut As HitAndBlowGame
+    <SetUp> Public Overridable Sub Setup()
+        sut = New HitAndBlowGame
+    End Sub
+
+    Public Class CountNumberOfHitTest : Inherits HitAndBlowGameTest
+        <Test()> Public Sub ヒット数を返すメソッドのテスト()
+            Dim playerNumber As New List(Of Char)(New Char() {"1"c, "5"c, "3"c, "0"c})
+            Dim computerNumber As New List(Of Char)(New Char() {"1"c, "5"c, "3"c, "4"c})
+
+
+            Dim result As Integer = sut.CountNumberOfHit(computerNumber, playerNumber)
+            Assert.That(result, [Is].EqualTo(3))
+
+        End Sub
+    End Class
+
+End Class
