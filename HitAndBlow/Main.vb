744ecb15954b1b9bdd407a471953623008a0a4ba
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-15 08:30:57 +0900

    コンソールアプリにゲームタイトルと結果表示

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -1,7 +1,10 @@
 ﻿Module Main
 
     Sub Main()
-
+        Console.WriteLine("ヒット＆ブロー")
+        Dim hitAndBlowGame As New HitAndBlowGame
+        Dim playerAswer As String = "1234"
+        Console.WriteLine(hitAndBlowGame.HitAndBlow(playerAswer))
     End Sub
 
 End Module
