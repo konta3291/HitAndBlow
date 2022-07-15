66ec0f79ac311605b51a13c89dece616444c8626
UEHARA Kouichi <k-uehara@daniel-soft.com>
2022-07-15 09:43:10 +0900

    ゲームクリア後のリプレイ処理追加

--- HitAndBlow/Main.vb
+++ HitAndBlow/Main.vb
@@ -3,10 +3,25 @@ Public Module Main
 
     Sub Main()
         Console.WriteLine("ヒット＆ブロー")
-        Dim hitAndBlowGame As New HitAndBlowGame
-        hitAndBlowGame.HitAndBlow()
-        Console.WriteLine("正解です！ゲームクリア！")
+        Do
+            Dim hitAndBlowGame As New HitAndBlowGame
+            hitAndBlowGame.HitAndBlow()
+            Console.WriteLine("正解です！ゲームクリア！")
+        Loop While AskPlayerWantToPlayAgain()
     End Sub
+
+    ''' <summary>
+    ''' もう一度ヒット＆ブローを遊ぶか聞きます
+    ''' </summary>
+    Private Function AskPlayerWantToPlayAgain() As Boolean
+        Dim playerAnswer As String
+
+        While Not ("Y".Equals(playerAnswer) OrElse "N".Equals(playerAnswer))
+            Console.Write("もう一度プレイしますか？（Y/N）:")
+            playerAnswer = Console.ReadLine().ToUpper
+        End While
+        Return playerAnswer.Equals("Y")
+    End Function
 
     ''' <summary>
     ''' プレイヤーからヒット＆ブローゲームで遊ぶための四桁の整数を受け取る
